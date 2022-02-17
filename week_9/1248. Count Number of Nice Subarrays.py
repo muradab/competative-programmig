0dfbc6d07991361e495930b5182a27e58db10674
muradab <se.murad.abdella@gmail.com>
2022-02-17 12:45:57 +0300

    Solutions to multiple leetcode problems pushed

---
+++ week_9/1248. Count Number of Nice Subarrays.py
@@ -0,0 +1,27 @@
+# Given an array of integers nums and an integer k. 
+# A continuous subarray is called nice if there are k odd numbers on it.
+
+# Return the number of nice sub-arrays.
+
+from typing import List 
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+
+        preCount = {}
+    
+        preCount[0] = 1
+        answer = 0
+        count = 0
+        for num in nums:
+            
+            if num % 2 != 0:
+                count += 1
+                preCount[count] = 0
+                
+            preCount[count] += 1
+            
+            
+            if count - k in preCount:
+                answer += preCount[count - k]
+            
+        return answer
