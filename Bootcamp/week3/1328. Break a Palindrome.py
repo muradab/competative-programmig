01346c4f27346c07647fe7ca84f0912c3ddda4e7
muradab <50866469+muradab@users.noreply.github.com>
2022-03-26 15:55:35 +0300

    Create 1328. Break a Palindrome.py

---
+++ Bootcamp/week3/1328. Break a Palindrome.py
@@ -0,0 +1,27 @@
+# Given a palindromic string of lowercase English letters palindrome, replace exactly one character with any lowercase English letter so that the resulting string is not a palindrome and that it is the lexicographically smallest one possible.
+
+# Return the resulting string. If there is no way to replace a character to make it not a palindrome, return an empty string.
+
+# A string a is lexicographically smaller than a string b (of the same length) if in the first position where a and b differ, a has a character strictly smaller than the corresponding character in b. For example, "abcc" is lexicographically smaller than "abcd" because the first position they differ is at the fourth character, and 'c' is smaller than 'd'.
+
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        length = len(palindrome)
+        if length < 2 :
+            return ""
+        
+        palindrome = list(palindrome)
+
+        for i in range(length//2):
+             if palindrome[i] != 'a':
+                palindrome[i] = 'a'
+                return ''.join(palindrome)
+        
+        palindrome[length-1] = 'b'
+        
+        return ''.join(palindrome)
+                
+#         return palindrome
+                
+        
+                
