c01dda5b3011296b1b4d551ee93ce666340fc749
muradab <50866469+muradab@users.noreply.github.com>
2022-04-18 13:56:11 +0300

    Update 234. Palindrome Linked List.py

--- week_9/234. Palindrome Linked List.py
+++ week_9/234. Palindrome Linked List.py
@@ -22,3 +22,26 @@ class Solution:
             right -= 1
             
         return True
+    
+#  # Definition for singly-linked list.
+# # class ListNode:
+# #     def __init__(self, val=0, next=None):
+# #         self.val = val
+# #         self.next = next
+# class Solution:
+#     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+#         prev = None
+#         slow = fast = head
+        
+#         while fast and  fast.next :
+#             fast = fast.next.next
+#             prev , prev.next , slow = slow , prev , slow.next
+        
+#         if fast :
+#             slow = slow.next
+        
+#         while prev and slow.val == prev.val :
+#             slow = slow.next
+#             prev = prev.next
+        
+#         return not slow
